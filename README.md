3c6fe1d26c1b2f1b0313d1ae8cb44b209662da3b
Aliaksei Harabchuk <aliaksei.harabchuk@gmail.com>
2021-01-17 20:56:50 +0100

    Added methods and documentation

--- README.md
+++ README.md
@@ -12,15 +12,102 @@ npm i @sipuni/usedesk
 
 ### Реализованные методы
 
-Методы сгруппированы и доступны через соответствующие свойства у объекта класса SipuniUsedeskApi.
+Методы сгруппированы и доступны через соответствующие свойства у объекта класса `SipuniUsedeskApi`,
+например, `api.clients.create()` или `api.tickets.list()`.
 Все методы возвращают Promise.
 
-Для списка методов ниже предположим, что amoApi это объект класса SipuniUsedeskApi
+Методы, принимающие `properties`, ожидают объект. В объекте укажите такие же поля как в [документации](https://usedeskkb.atlassian.net/wiki/spaces/API/overview), 
+кроме поля `api_token`, оно подается автоматически при каждом вызове метода.
+
+Для списка методов ниже, предположим, что `api` это объект класса `SipuniUsedeskApi`
 ```ecmascript 6
 const SipuniUsedeskApi = require('@sipuni/usedesk');
-const usedeskApi = new SipuniUsedeskApi(..)
+const token = 'aed2e810..........f05e2e21a';
+const api = new SipuniUsedeskApi({ token });
 ```
 
+Тикеты
+```ecmascript 6
+api.tickets.create(ticketProperties)
+api.tickets.get(ticketId)
+api.tickets.update(ticketProperties)
+api.tickets.list(properties)
+api.tickets.createComment(commentProperties)
+api.tickets.tags()
+api.tickets.fields()
+```
+
+Клиенты
+```ecmascript 6
+api.clients.create(clientProperties)
+api.clients.get(clientId)
+api.clients.update(clientProperties)
+api.clients.list(properties)
+```
+
+Агенты
+```ecmascript 6
+api.users.groups()
+api.users.create(userProperties);
+api.users.get(userId);
+api.users.update(userProperties);
+api.users.list(properties);
+api.users.delete(userId);
+```
+
+### Примеры
+
+Для коробочной версии
+```ecmascript 6
+const SipuniUsedeskApi = require('@sipuni/usedesk');
+const token = 'aed2e810..........f05e2e21a';
+const host = 'api.yourdomain.com';
+const api = new SipuniUsedeskApi({ token, host });
+```
+
+Для облачной версии
+```ecmascript 6
+const SipuniUsedeskApi = require('@sipuni/usedesk');
+const token = 'aed2e810..........f05e2e21a';
+const api = new SipuniUsedeskApi({ token });
+```
+
+Создание тикета
+```ecmascript 6
+const created = await api.tickets.create({
+  subject: 'Не работает сеть',
+  message: 'Подробности...'
+});
+```
+
+Получение тикета по id
+```ecmascript 6
+const ticket = await api.tickets.get(ticket_id);
+```
+
+Создание клиента
+```ecmascript 6
+const client = await api.clients.create({ 
+  name: 'Иван',
+  emails: ['test@gmail.com'],
+});
+```
+
+Получение клиента по id
+```ecmascript 6
+const found = await api.clients.get(client_id);
+```
+
+Создание агента
+```ecmascript 6
+const groups = await api.users.groups();
+const user = await api.users.create({
+    name: 'Николй',
+    email: 'abc@cde.com',
+    password: '123',
+    group: `${groups[0].id}`,
+});
+```
 
 ### Лицензия
 
--- index.js
+++ index.js
@@ -1,14 +1,221 @@
 const axios = require('axios');
 
+const DEFAULT_API_HOST = 'api.usedesk.ru';
 
-class SipuniUsedesk {
-  options = {};
+const valueOrNull = (val) => val ? val : null;
 
-  constructor(options) {
-    this.options = options;
+class TicketsMethods {
+  constructor (api) {
+    this.api = api;
   }
 
+  async get(ticket_id, accessible_for_agent_id=null) {
+    const properties = {
+      ticket_id,
+    };
+    if (accessible_for_agent_id) {
+      properties.accessible_for_agent_id = accessible_for_agent_id;
+    }
+    return this.api.request('POST', `/ticket`, properties);
+  }
+
+  async create(properties) {
+    return this.api.request('POST', '/create/ticket', properties);
+  }
+
+  async createAsync(properties) {
+    return this.api.request('POST', '/create/ticket/async', properties);
+  }
+
+  async update(properties) {
+    return this.api.request('POST', '/update/ticket', properties);
+  }
+
+  async list(properties) {
+    return this.api.request('POST', '/tickets', properties);
+  }
+
+  async createComment(properties) {
+    return this.api.request('POST', '/create/comment', properties);
+  }
+
+  async tags(query='', offset= 0) {
+    const properties = {
+      offset,
+    };
+    if (query) {
+      properties.query = query;
+    }
+    return this.api.request('POST', '/tags', properties);
+  }
+
+  async fields() {
+    return this.api.request('POST', '/ticket/fields');
+  }
 }
 
 
-module.exports = SipuniUsedesk;
+class ClientsMethods {
+  constructor (api) {
+    this.api = api;
+  }
+
+  async get(client_id) {
+    return this.api.request('GET', `/client`, { client_id });
+  }
+
+  async create(properties) {
+    return this.api.request('POST', `/create/client`, properties);
+  }
+
+  async update(properties) {
+    return this.api.request('POST', `/update/client`, properties);
+  }
+
+  async list(properties) {
+    return this.api.request('POST', `/clients`, properties);
+  }
+}
+
+
+class ChatMethods {
+  constructor (api) {
+    this.api = api;
+  }
+
+  async addMessage(properties) {
+    return this.api.request('POST', '/chat/addMessage', properties);
+  }
+
+  async sendMessage(properties) {
+    return this.api.request('POST', '/chat/sendMessage'. properties);
+  }
+
+  async changeAssignee(properties) {
+    return this.api.request('POST', '/chat/changeAssignee'. properties);
+  }
+}
+
+
+class UsersMethods {
+  constructor (api) {
+    this.api = api;
+  }
+
+  async get(user_id) {
+    return this.api.request('GET', '/user', { user_id });
+  }
+
+  async create(properties) {
+    return this.api.request('POST', '/create/user', properties);
+  }
+
+  async delete(user_id) {
+    return this.api.request('POST', '/delete/user', { user_id });
+  }
+
+  async update(properties) {
+    return this.api.request('POST', '/update/user', properties);
+  }
+
+  async list(properties) {
+    return this.api.request('POST', '/users', properties);
+  }
+
+  async groups() {
+    return this.api.request('POST', '/groups');
+  }
+}
+
+
+class ChannelsMethods {
+  constructor (api) {
+    this.api = api;
+  }
+
+  async list(properties) {
+    return this.api.request('GET', '/channels', properties);
+  }
+}
+
+
+class SipuniUsedeskApi {
+  options = {};
+  cachedMethodsObjects = {};
+
+  constructor(options) {
+    this.options = options;
+    // validation
+    if (!options.token) {
+      throw new Error('A required "token" parameter is not set.');
+    }
+    if (!options.host) {
+      this.options.host = DEFAULT_API_HOST;
+    }
+  }
+
+  // API methods grouped by entity
+
+  get clients() {
+    return this._getMethodsObject(ClientsMethods);
+  }
+
+  get tickets() {
+    return this._getMethodsObject(TicketsMethods);
+  }
+
+  get chat() {
+    return this._getMethodsObject(ChatMethods);
+  }
+
+  get users() {
+    return this._getMethodsObject(UsersMethods);
+  }
+
+  get channels() {
+    return this._getMethodsObject(ChannelsMethods);
+  }
+
+  //Universal request
+
+  async request(method, path, paramsOrData = {}) {
+    const isGet = method === 'GET';
+
+    const paramsOrDataWithToken = {
+      ...paramsOrData,
+      api_token: this.options.token,
+    };
+
+    const params = isGet ? paramsOrDataWithToken : {};
+    const data = !isGet ? paramsOrDataWithToken : {};
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+
+    try {
+      const response = await axios.request({
+        url: `https://${this.options.host}${path}`,
+        method,
+        data,
+        params,
+        headers,
+      });
+      return valueOrNull(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+      throw error;
+    }
+  }
+
+  // Utility methods
+
+  _getMethodsObject(className, options = {}) {
+    if (!this.cachedMethodsObjects[className]) {
+      this.cachedMethodsObjects[className] = new className(this, options);
+    }
+    return this.cachedMethodsObjects[className];
+  }
+}
+
+
+module.exports = SipuniUsedeskApi;
